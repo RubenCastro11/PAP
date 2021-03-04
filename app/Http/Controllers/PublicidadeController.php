2aa8be661f0e94dab740874f5e2557a22c65dfd0
Noel3700 <72562565+Noel3700@users.noreply.github.com>
2021-03-04 15:29:28 +0000

    mota publicidade

--- app/Http/Controllers/PublicidadeController.php
+++ app/Http/Controllers/PublicidadeController.php
@@ -25,17 +25,59 @@ class PublicidadeController extends Controller
      public function create(){
            return view('publicidade.create');
      }
+
+
+
+
+
+
       public function store(Request $request){
         $novoPublicidade=$request->validate([
             'id_mota'=>['required','numeric'],
             'designacao'=>['required','min:0'],
-            'fotografia'=>['required','image'],
+            'fotografia'=>['required','image','max:2000'],
             
           ]);
-        $publicidade = Publicidade::create($novoPublicidade);
+        
 
+        if ($request->hasFile('fotografia')){
+            $nomeimagem = $request->file('fotografia')->getClientOriginalName();
+            
+            $nomeImagem = time(). '_'. $nomeimagem;
+            $guardarImagem = $request->file('fotografia')->storeAs('imagens/publicidade',$nomeImagem);
+            
+            $novoPublicidade['fotografia']=$nomeImagem;
+
+        }
+        $publicidade = Publicidade::create($novoPublicidade);
         return redirect()->route('publicidades.show',[
           'id'=>$publicidade->id_publicidade
         ]);
+}
+
+
+
+        public function update (Request $request){
+            $idpublicidade=$request->id;
+            $publicidade=Publicidade::findOrFail($idpublicidade);
+
+            $atualizarpublicidade=$request->validate([
+
+            'id_mota'=>['required','numeric'],
+            'designacao'=>['required','min:0'],
+            'fotografia'=>['required','image','max:2000'],
+            ]);
+            if($request->hasFile('imagem_capa')){
+                $nomeImagem=$request->file('fotografia')->getClientOriginalName();
+                $nomeImagem=time().'_'.$nomeImagem;
+                $guardarImagem=$request->file('fotografia')->storeAs('imagens/publicidade',$nomeImagem);
+                $atualizarMota['fotografia']=$nomeImagem;
+            }
+            $publicidade->update($editPublicidade);
+        
+        return redirect()->route('publicidades.show',[
+            'id'=>$publicidade->id_publicidade
+        ]);
         }
- }
+    }
+ 
